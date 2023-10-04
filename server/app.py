104df6bac9bf75efdf40ecd7fc4b375647c0a771
Kamyar Taghavi <caracicatriz.kt@gmail.com>
2023-10-04 17:29:07 -0600

    Completed lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,49 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by('created_at').all()
+        response = make_response(
+            jsonify([message.to_dict() for message in messages]),200
+            )
+    elif request.method == 'POST':
+        data = request.get_json()
+        message = Message(
+            body=data['body'],
+            username = data['username']
+        )
+        db.session.add(message)
+        db.session.commit()
+        response = make_response(
+            jsonify(message.to_dict()), 201
+        )
 
-@app.route('/messages/<int:id>')
+    return response
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method == 'PATCH':
+        newMessageData = request.get_json()
+        for key in newMessageData:
+            setattr(message, key, newMessageData[key])
+
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(jsonify(message.to_dict()), 200)
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response = ''
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555)
